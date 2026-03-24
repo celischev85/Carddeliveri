88f0f510a0c0fcb31f0fe773574a8f745930bf45
Dima <celischev85@gmail.com>
2026-03-24 23:08:54 +0300

    initil commit

--- src/test/java/ru/netology/CarddeliveriTest.java
+++ src/test/java/ru/netology/CarddeliveriTest.java
@@ -1,7 +1,6 @@
 package ru.netology;
 
 import com.codeborne.selenide.Condition;
-import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.Keys;
 
